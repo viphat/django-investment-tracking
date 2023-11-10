49dac45d1344c007558b81813bf3845612fb306f
Duong Vi Phat <viphat@gmail.com>
2023-11-10 18:09:00 +0900

    Init Project

---
+++ assets/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Category(models.Model):
+  notionId = models.CharField(max_length=255, blank=False, null=False)
+  name = models.CharField(max_length=255, blank=False, null=False)
+  color = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+class Tag(models.Model):
+  notionId = models.CharField(max_length=255, blank=False, null=False)
+  name = models.CharField(max_length=255, blank=False, null=False)
+  color = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+class InvestmentRecord(models.Model):
+  notionId = models.CharField(max_length=255, blank=False, null=False)
+  itemName = models.TextField(blank=False, null=False)
+  itemDescription = models.TextField(blank=True, null=True)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=False, null=False)
+  tags = models.ManyToManyField(Tag)
+  date = models.DateField(blank=False, null=False)
+  currency = models.CharField(max_length=3, blank=False, null=False)
+  amount = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+  profitLoss = models.DecimalField(max_digits=10, decimal_places=2)
+  year = models.IntegerField(blank=False, null=False)
+
+  def __str__(self):
+    return "{p1}: {p2}".format(p1=self.itemName, p2=self.amount)
+
+class Report(models.Model):
+  key = models.CharField(max_length=255, blank=False, null=False)
+  value = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
